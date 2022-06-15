5ea706b54681ad72ebbd6feaa38a5f8d672e73fe
Marcus Krassmann <marcus.krassmann@gmail.com>
2022-06-15 20:36:40 +0200

    fixed indention

--- cli-client/src/test/java/letspokercliclient/LetsPokerTerminalTest.java
+++ cli-client/src/test/java/letspokercliclient/LetsPokerTerminalTest.java
@@ -24,27 +24,27 @@ class LetsPokerTerminalTest {
     void runInternal() {
         mockTextTerminal.getInputs().addAll(List.of("woozah", "woopas", "23", "3"));
         String expected = """
-Let's play Poker!
-=================
-You are the Button. Do you want to Fold, Call or raise to amount X?
-Username [admin]: woozah
-Password: woopas
-Age: 23
-What month were you born in?
-1: JANUARY
-2: FEBRUARY
-3: MARCH
-4: APRIL
-5: MAY
-6: JUNE
-7: JULY
-8: AUGUST
-9: SEPTEMBER
-10: OCTOBER
-11: NOVEMBER
-12: DECEMBER
-Enter your choice: 3
-User woozah is 23 years old, was born in MARCH and has the password woopas.""";
+            Let's play Poker!
+            =================
+            You are the Button. Do you want to Fold, Call or raise to amount X?
+            Username [admin]: woozah
+            Password: woopas
+            Age: 23
+            What month were you born in?
+            1: JANUARY
+            2: FEBRUARY
+            3: MARCH
+            4: APRIL
+            5: MAY
+            6: JUNE
+            7: JULY
+            8: AUGUST
+            9: SEPTEMBER
+            10: OCTOBER
+            11: NOVEMBER
+            12: DECEMBER
+            Enter your choice: 3
+            User woozah is 23 years old, was born in MARCH and has the password woopas.""";
 
         testee.runInternal();
 
